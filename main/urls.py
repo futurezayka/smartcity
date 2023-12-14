9e9079f2cd74ae56d4b71a458b94f8604e8f6fbe
futurezayka <kurachvlad911@gmail.com>
2023-12-14 23:57:20 +0200

    staticfiles fix

--- main/urls.py
+++ main/urls.py
@@ -3,10 +3,13 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path
 
 from main.views import *
+from smartcity import settings
 from smartcity.settings import MEDIA_URL, MEDIA_ROOT
 
 urlpatterns = [
     path('', index, name='home')
 ]
+
 urlpatterns += staticfiles_urlpatterns()
 urlpatterns += static(MEDIA_URL, document_root=MEDIA_ROOT)
+
